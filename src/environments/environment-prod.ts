1073ba2de5f43f42ba7ec10d6f6c8a21e1c891f1
Edwin Lima <edwin.limaa@gmail.com>
2024-04-22 11:07:57 -0300

    Set Herorku address api configuration

--- src/environments/environment-prod.ts
+++ src/environments/environment-prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    production: true
+    production: true,
+    API_URL: "https://app-selecao-pitang-backend-7b581e9ba075.herokuapp.com/api"
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 export const environment = {
     production: false,
-    API_URL:'http://localhost:8080/api'
+    API_URL:'https://app-selecao-pitang-backend-7b581e9ba075.herokuapp.com/api'
   };
 /*
  * For easier debugging in development mode, you can import the following file
